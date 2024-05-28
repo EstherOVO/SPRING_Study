1f338bbe64e6c9d19712a31b3a706d7e5e4b99eb
EstherOVO <tmejwjd@gmail.com>
2024-05-28 16:48:29 +0900

    Spring

---
+++ src/main/java/com/busanit/SPRING_Study/SpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.busanit.SPRING_Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+
+}
